d6bb370533f0c4db35ac53e11071b139c9cfbb89
Siddharth Patel <sidsiddharthpatel@gmail.com>
2019-06-07 22:44:53 +0530

    UI

---
+++ calcUI.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    printf(" _________________________ \n");//25 underscores
+    printf("│ _______________________ │\n");
+    printf("││                       ││\n"); //put a loop here and
+    printf("│ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ │\n");
+    printf("│     _______________     │\n");
+    printf("│    │ + │ - │ × │ ÷ │    │\n"); //read the arrows
+    printf("│     ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯     │\n");
+    printf(" ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ \n");
+
+    return 0;
+}
