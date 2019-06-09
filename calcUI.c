b17475ca2d2e3a3a1a8babcfae698a8c3c88d9e6
Siddharth Patel <sidsiddharthpatel@gmail.com>
2019-06-09 11:52:26 +0530

    Update calcUI.c

--- calcUI.c
+++ calcUI.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int main()
+int calui()
 {
     printf(" _________________________ \n");//25 underscores
     printf("│ _______________________ │\n");
@@ -11,4 +11,54 @@ int main()
     printf(" ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ \n");
 
     return 0;
+}
+
+void Exchange(int a, int b)
+{
+    int temp;
+    temp = arrow[a];
+    arrow[a] = arrow[b];
+    arrow[b] = temp;
+}
+
+void reading()
+{
+    int ch, value = 0;
+    ch = getch ();
+    if (ch == 0 || ch == 224)
+    {
+        switch (getch ())
+        {
+            case 75://LeftArrow
+            if(y>0)
+            {
+                Exchange(y, y-1);
+                y = y-1;
+            }
+            else
+                printf("\a");
+            return value;
+            break;
+
+            case 77://RightArrow
+            if(y<3)
+            {
+                Exchange(y, y+1);
+                y = y+1;
+            }
+            else
+                printf("\a");
+            return value;
+            break;
+
+            default :
+            value = 1;
+            return value;
+        }
+        else if (ch == 'q' || ch == 'Q')
+        {
+                value = 1;
+                return value;
+         }
+    }
 }
